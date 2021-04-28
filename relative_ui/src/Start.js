693c0481e56d93a40673a423b66d636158ef064d
Anton <antonbol@kth.se>
2021-04-28 14:21:16 +0200

    fixed passing of relativeCode from start to call

--- relative_ui/src/Start.js
+++ relative_ui/src/Start.js
@@ -1,25 +1,32 @@
-import React from "react";
-
+import React,{useState} from "react";
+import {Link} from "react-router-dom";
 
 function Start() {
+    const [relativeCode,setRelativeCode] = useState(null);
+    const handleCodeChange = (e) =>{
+        setRelativeCode(e.target.value);
+    }
     return (
+    
     <div>
-        <div className="center paddingheader">
-            <h1 className='headerstyle'>Dogood</h1>
-            <link rel="stylesheet" href="styleOne.css" />
-        </div>
+            <div className="center paddingheader">
+                <h1 className='headerstyle'>Dogood</h1>
+                <link rel="stylesheet" href="styleOne.css" />
+            </div>
 
-
-        <div className="center">
-            <form action="/Call">
-                <label for = "code">Skriv in kod du fått per sms:</label><br/>
-                <input type = "text" id = "code"></input><br/>
-                <input type = "submit" value = "Öppna samtal"></input>
-            </form>
-        </div>
+            <div className="center">
+                <form> 
+                    <label htmlFor = "code">Skriv in kod du fått per sms:</label><br/>
+                    <input type = "text" id = "code" placeholder="t.ex 123-456" value={relativeCode} onChange={handleCodeChange} required></input><br/>
+                    <Link to ={{pathname:"/Call",relativeCode:relativeCode}}><input type = "submit" value = "Öppna samtal"></input></Link>
+                </form>
+            </div>
+        
+        
     </div>
 
     );
 }
+
 
 export default Start;
