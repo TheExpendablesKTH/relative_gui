5887d3c0cbdc14fad22d32a426635d4c98321b0f
torarvill <arvill@kth.se>
2021-05-01 16:14:08 +0200

    added popup if code is incorrectly formatted

--- src/Start.js
+++ src/Start.js
@@ -1,13 +1,23 @@
 import React,{useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
 
 function Start() {
     const [relativeCode,setRelativeCode] = useState(null);
+    const [approvedCode,setApprovedCode] = useState(false);
+    const codeRegex = /^[A-Z0-9]{3}\-[A-Z0-9]{3}$/;
     const handleCodeChange = (e) =>{
         setRelativeCode(e.target.value);
     }
+    const checkCode = (e) => {
+      e.preventDefault();
+      if(codeRegex.test(relativeCode)){
+          setApprovedCode(true)
+      }else{
+        window.alert("Felaktig kod.\n\nBehöver vara på formen: XXX-XXX \n\nSamt endast stora bokstäver och siffor");
+      }
+    };
     return (
-    
+
     <div>
             <div className="center paddingheader">
                 <h1 className='headerstyle'>Dogood</h1>
@@ -15,18 +25,19 @@ function Start() {
             </div>
 
             <div className="center">
-                <form> 
+                <form onSubmit={checkCode}>
                     <label htmlFor = "code">Skriv in kod du fått per sms:</label><br/>
                     <input type = "text" id = "code" placeholder="t.ex 123-456" value={relativeCode} onChange={handleCodeChange} required></input><br/>
-                    <Link to ={{pathname:"/Call",relativeCode:relativeCode}}><input type = "submit" value = "Öppna samtal"></input></Link>
+                    <input type = "submit" value = "Öppna samtal"></input>
                 </form>
             </div>
-        
-        
+            {approvedCode && <Redirect to={{pathname:"/Call",relativeCode:relativeCode}}/>}
+
+
     </div>
 
     );
-}
 
+}
 
 export default Start;
