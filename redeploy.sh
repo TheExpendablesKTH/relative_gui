7fcebc20c301d856f7e432b19a7358abbe255f07
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2021-05-02 01:16:53 +0200

    update paths to point to relative_gui instead of frontend

--- redeploy.sh
+++ redeploy.sh
@@ -3,6 +3,6 @@ echo "redeploying..."
 
 sudo -u ubuntu git pull
 npm run build
-sudo rm -rf /var/www/html/frontend
-sudo mkdir /var/www/html/frontend
-sudo cp -R build/. /var/www/html/frontend
+sudo rm -rf /var/www/html/relative_gui
+sudo mkdir /var/www/html/relative_gui
+sudo cp -R build/. /var/www/html/relative_gui
